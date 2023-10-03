8eb427939e95fedd2333da2984c3d3e6e8f838f0
lazarusking <vegetasssuper@gmail.com>
2023-10-03 21:53:36 +0000

    removed some css

--- frontend/src/context/user.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReactNode, useContext } from "react";
-
-const UserContext = React.createContext({});
-
-const UserProvider = ({ children }: { children: ReactNode }) => {};
-
-export const useAccount = () => useContext(UserContext);
